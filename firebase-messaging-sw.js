ca09a69c4c013de102426740ff05bec116cbaa77
Divyansh Bajpai <divyanshbajpai27@gmail.com>
2026-03-11 00:19:06 +0530

    Initial chat system with Firebase and push notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js",
+);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBVOPQzsrPnZBKnuQhKOdOFWxNFJh9Ae-A",
+  authDomain: "death-note-3ff99.firebaseapp.com",
+  projectId: "death-note-3ff99",
+  messagingSenderId: "86048543927",
+  appId: "1:86048543927:web:31a7b84984a25809066932",
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+  const title = payload.notification.title;
+
+  const options = {
+    body: payload.notification.body,
+    icon: "icon.png",
+  };
+
+  return self.registration.showNotification(title, options);
+});
---
+++ functions/index.js
@@ -0,0 +1,34 @@
+const { onValueCreated } = require("firebase-functions/v2/database");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+exports.sendMessageNotification = onValueCreated(
+  {
+    ref: "/messages/{msgId}",
+    region: "us-central1",
+    instance: "death-note-3ff99-default-rtdb",
+  },
+  async (event) => {
+    const msg = event.data.val();
+
+    const payload = {
+      notification: {
+        title: msg.name + " says:",
+        body: msg.text || msg.fileName || "Sent a file",
+        icon: "/icon.png",
+      },
+    };
+
+    const tokensSnap = await admin.database().ref("pushTokens").once("value");
+
+    const tokens = [];
+    tokensSnap.forEach((child) => {
+      tokens.push(child.val().token);
+    });
+
+    if (tokens.length === 0) return;
+
+    await admin.messaging().sendToDevice(tokens, payload);
+  },
+);
